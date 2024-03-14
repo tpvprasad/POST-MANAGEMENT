86ce23a6ea19e6c3ee917c0907ddb1534a03e4e2
BandaruHarsha <tpvprasad98@gmail.com>
2024-03-14 11:06:45 +0530

    commit

--- src/ProtectedLayout.jsx
+++ src/ProtectedLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-function ProtectedLayout({ children, authentication}) {
+function ProtectedLayout({ children, authentication }) {
   const [loader, setLoader] = useState(true);
 
   const authStatus = useSelector((state) => state.auth.status);
@@ -14,7 +14,7 @@ function ProtectedLayout({ children, authentication}) {
     } else if (!authentication && authStatus !== authentication) {
       navigate("/");
     }
-    setLoader(false)
+    setLoader(false);
   }, [authStatus, authentication, navigate]);
 
   return loader ? null : <div>{children}</div>;
